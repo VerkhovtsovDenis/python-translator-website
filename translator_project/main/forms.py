1781a6e5e5d32fdd1693bc442802ec18bda5c64a
VerkhovtsovDenis <verkhovtcov.do@dvfu.ru>
2025-05-08 16:19:10 +1000

    chore: upd form - add language field

--- translator_project/main/forms.py
+++ translator_project/main/forms.py
@@ -1,11 +1,18 @@
 from django import forms
+from main.models import SupportLanguage
 
 
-class PascalCodeForm(forms.Form):
-    pascal_code = forms.CharField(widget=forms.Textarea(
-        attrs={
-            'class': "form-control prettyprint",
-            'rows': "15",
-            "placeholder": "Введите ваш код на Pascal..."}),
-        label='',
-        max_length=5000)
+class TranslateForm(forms.Form):
+    input_code = forms.CharField(
+        widget=forms.Textarea(attrs={
+            'class': 'form-control prettyprint',
+            'rows': 15,
+            'placeholder': 'Введите ваш код на Pascal...'
+        }),
+        label=''
+    )
+
+    language = forms.CharField(
+        widget=forms.HiddenInput(),
+        initial='python'
+    )
