a8d67b651fdccc9e27450bf3b51b4735c1763b65
VerkhovtsovDenis <verkhovtcov.do@dvfu.ru>
2025-05-08 16:18:28 +1000

    add models in admin site

--- translator_project/main/admin.py
+++ translator_project/main/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import SupportLanguage, History
 
-# Register your models here.
+
+@admin.register(SupportLanguage)
+class SupportLanguageAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(History)
+class HistoryAdmin(admin.ModelAdmin):
+    list_display = ('ip_address', 'req_date', 'language')
+    list_filter = ('language', 'req_date')
+    search_fields = ('input_code', 'output_code')
+    readonly_fields = ('req_date',)
